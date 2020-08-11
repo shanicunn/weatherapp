97476f091b3a50a3bd8faedd66d47177238da432
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-10 22:28:36 -0400

    Displaying hourly weather but not for Sunday

--- src/Components/Sunday.js
+++ src/Components/Sunday.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 
-const API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=";
+const API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.7490000&lon=-84.3879800&&units=imperial&exclude=current,minutely&appid=";
 const API_KEY = "ff74cea309cf651b835e1f362b1a5897";
+
 
 class Sunday extends React.Component {
     constructor() {
@@ -11,12 +12,19 @@ class Sunday extends React.Component {
             info: [],
         }
     }
-   
+    militaryTime (time) {
+        let dt = new Date(time * 1000);
+        let hr = dt.getHours();
+        let m = "0" + dt.getMinutes();
+        let newTime = hr + ":" + m.substr(-2);
+        return newTime;
+    }
+
     async getSunday() {
         try {
             const res = await axios.get(API_URL + API_KEY);
             this.setState({
-                info: res.data,
+                info: res.data.hourly,
             }) 
         } catch {
                 console.log("Error");
@@ -30,7 +38,17 @@ class Sunday extends React.Component {
     render() {
         return (
             <div>
-                {this.getSunday()};
+                 {this.state.info.map(forecast => {
+                    {/* console.log(forecast); */}
+                    return (
+                        <div className="forecast">
+                        <h1>{this.militaryTime(forecast.dt)}</h1>
+                        <h1>{forecast.temp}</h1>
+                        <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt=""></img>
+                        <h3>{forecast.weather[0].description}</h3>
+                        </div>
+                    )
+                })}
             </div>
     )
     }
